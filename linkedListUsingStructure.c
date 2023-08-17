a275bdbc4432e43d1163122cd7c8aa20ab9742de
Debjit0001 <84177399+Debjit0001@users.noreply.github.com>
2023-08-17 20:53:15 +0530

    Update linkedListUsingStructure.c

--- linkedListUsingStructure.c
+++ linkedListUsingStructure.c
@@ -6,24 +6,30 @@ typedef struct listNode {
     struct listNode* next;
 } node;
 
-// Define struct LinkedList before typedefs
+// Defining struct struct linkedList before typedefs
 struct linkedList;
 
 typedef void (*insertAtFunc)(struct linkedList*, int, int);
 typedef void (*insertFirstFunc)(struct linkedList*, int);
 typedef void (*insertLastFunc)(struct linkedList*, int);
 typedef void (*deleteAtFunc)(struct linkedList*, int);
+typedef void (*deleteFirstFunc)(struct linkedList*);
+typedef void (*deleteLastFunc)(struct linkedList*);
+typedef void (*reverseFunc)(struct linkedList*);
 
-typedef struct linkedList {
+struct linkedList {
     int size;
     node *head, *tail;
     insertAtFunc insertAt;
     insertFirstFunc insertFirst;
     insertLastFunc insertLast;
     deleteAtFunc deleteAt;
-} LinkedList;
+    deleteFirstFunc deleteFirst;
+    deleteLastFunc deleteLast;
+    reverseFunc reverse;
+};
 
-void insertFirst(LinkedList* list, int value) {
+void insertFirst(struct linkedList* list, int value) {
     node* newNode = malloc(sizeof(node));
     newNode->value = value;
     newNode->next = NULL;
@@ -40,7 +46,7 @@ void insertFirst(LinkedList* list, int value) {
     ++list->size;
 }
 
-void insertLast(LinkedList* list, int value) {
+void insertLast(struct linkedList* list, int value) {
     node* newNode = malloc(sizeof(node));
     newNode->value = value;
     newNode->next = NULL;
@@ -56,7 +62,7 @@ void insertLast(LinkedList* list, int value) {
     ++list->size;
 }
 
-void insertAt(LinkedList* list, int value, int index) {
+void insertAt(struct linkedList* list, int value, int index) {
     if(index < 0 || index > list->size)
         return;
 
@@ -82,7 +88,7 @@ void insertAt(LinkedList* list, int value, int index) {
     currNode -> next = newNode;
 }
 
-void deleteAt(LinkedList* list, int index) {
+void deleteAt(struct linkedList* list, int index) {
     if(index < 0 || index > list->size)
         return;
 
@@ -106,6 +112,31 @@ void deleteAt(LinkedList* list, int index) {
     --list->size;
 }
 
+void deleteFirst(struct linkedList* list) { deleteAt(list, 0); }
+
+void deleteLast(struct linkedList* list) { deleteAt(list, list->size); }
+
+void reverse(struct linkedList *list) {
+    if(list -> size < 2) {
+        return;
+    }
+    list -> tail = list -> head;
+
+    node* prev = NULL;
+    node* present = list -> head;
+    node* next = present -> next;
+
+    while(present != NULL) {
+        present -> next = prev;
+        prev = present;
+        present = next;
+        if(next != NULL) 
+            next = next -> next;
+    }
+
+    list -> head = prev;
+}
+
 // Printing the linked list
 void printList(struct linkedList *list) {
     node *currNode = list -> head;
@@ -119,18 +150,21 @@ void printList(struct linkedList *list) {
 }
 
 // Initializing the linked list
-void initList(LinkedList *list) {
+void initList(struct linkedList *list) {
     list -> size = 0;
     list -> head = NULL;
     list -> insertAt = insertAt;
     list -> insertFirst = insertFirst;
     list -> insertLast = insertLast;
     list -> deleteAt = deleteAt;
+    list -> deleteFirst = deleteFirst;
+    list -> deleteLast = deleteLast;
+    list -> reverse = reverse;
 }
 
 int main(int argc, char const *argv[])
 {
-    LinkedList list;
+    struct linkedList list;
     initList(&list);
 
     list.insertFirst(&list, 10);
@@ -141,8 +175,13 @@ int main(int argc, char const *argv[])
     list.insertAt(&list, 15, 2);
     printList(&list);
 
-    list.deleteAt(&list, 2);
+    // list.deleteAt(&list, 2);
+    // list.deleteLast(&list);
+    // printList(&list);
+
+    list.reverse(&list);
     printList(&list);
+    printf("%d\n", list.tail->value);
     
     return 0;
 }
